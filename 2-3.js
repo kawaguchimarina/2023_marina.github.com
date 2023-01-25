06e4634d8f8f3d5787a5308589da6592be5b765e
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 22:34:59 +0900

    Update 2-3.js

--- 2-3.js
+++ 2-3.js
@@ -1,1 +1,3 @@
+function imgchange4() {document.getElementById('huziwara').src ="./image/2019081.jpg";}
+function imgchange5() {document.getElementById('huziwara').src ="./image/ph_player_2022_1.jpg";}
 
