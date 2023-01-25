0af7650e2b62ebe9a5cca0320db4968f46e7f6ae
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 22:37:28 +0900

    Update 2-3.js

--- 2-3.js
+++ 2-3.js
@@ -1,3 +1,3 @@
-function imgchange7() {document.getElementById('matsukawa').src ="./image/";}
+function imgchange7() {document.getElementById('matsukawa').src ="./image/show_img.jpeg";}
 function imgchange8() {document.getElementById('matsukawa').src ="./image/ph_player_2022_2_2.jpg";}
 
