5af2930da5dcb45baeb90c47ce655bd19eee4beb
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 22:45:05 +0900

    Update 2-4.js

--- 2-4.js
+++ 2-4.js
@@ -1,1 +1,2 @@
-
+function imgchange10() {document.getElementById('sasaki').src ="./image/Roki.sasaki_marines.jpg";}
+function imgchange11() {document.getElementById('sasaki').src ="./image/ph_player_2022_17.jpg";}
