f388f83e9d2bac194caa92b7392e5cc8cda80f82
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 15:40:47 +0900

    Update 1-2.js

--- 1-2.js
+++ 1-2.js
@@ -1,4 +1,4 @@
 function imgchange2() 
 {
-document.getElementById('tahara').src='./image/1538454731_1538454710_001-x828-y552.jpg';
+document.getElementById('tahara').src='./image/o0745108014500203930.jpg';
 }
