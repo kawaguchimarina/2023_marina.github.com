9a5765457b948cded830991b008cc1e39b750556
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 17:58:24 +0900

    Update 1-6.js

--- 1-6.js
+++ 1-6.js
@@ -1,8 +1,8 @@
-function imgchange6(){
-if(document.getElementById('answer2')) {
- alert('はずれ!');
+function nazo1() {
+if (document.getElementById('answer1').value=='桜井和寿') {
+ alert('あたり!');
 }
 else {
- alert('あたり!');
+ alert('はずれ!');
  }
-}
+} 
