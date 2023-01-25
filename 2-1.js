7cabe8953041a1123e7c85b19caee2b207bfb2e0
kawaguchimarina <103973939+kawaguchimarina@users.noreply.github.com>
2023-01-25 19:00:56 +0900

    Update 2-1.js

--- 2-1.js
+++ 2-1.js
@@ -1,2 +1,2 @@
-function imgchange1() {document.getElementById('wada').src ="./image/22shisetsu.jpg";}
+function imgchange1() {document.getElementById('wada').src ="./image/202008170032-spnaviow_2020081701050_view.jpg";}
 function imgchange2() {document.getElementById('wada').src ="./image/ph_player_2022_63.jpg";}
